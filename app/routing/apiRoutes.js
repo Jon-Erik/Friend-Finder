bbdd9198528359c615f85925401bacf97421602f
Jon-Erik <jdc151@case.edu>
2018-04-25 11:36:21 -0400

    friend finding code working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,46 +1,63 @@
 var path = require("path");
-var friends = require("../data/friends.js");
 var fs = require("fs");
 
 module.exports = function(app) {
 	app.get("/api/friends", function(req, res) {
-		res.sendFile(path.join(__dirname, "../data/friends.js"));
+		res.sendFile(path.join(__dirname, "../data/friends.txt"));
 	})
 
 	app.post("/api/friends", function(req, res) {
-		var newfriend = req.body;
-		console.log(newfriend);
-		console.log(newfriend.scores[0]);
+		var newFriend = req.body;
+
+		var friendsArray;
+
+		fs.readFile(path.join(__dirname,'../data/friends.txt'), 'utf8', function(err, data) {
+		if (err) {
+			return console.error(err);
+		}
+
+		friendsArray = JSON.parse(data);
 
 		var friendMatch;
 		var lowestDifference;
-		
 
-		for (i = 0; i < friends.length; i++) {
-			var sumDifference;
+		for (i = 0; i < friendsArray.length; i++) {
+			var sumDifference = 0;
 
-			for (j = 0; j < friends[i].scores.length; i++) {
-				var addedDifference = Math.abs(newfriend.scores[j] - friends[j].scores[j]);
+			for (j = 0; j < friendsArray[i].scores.length; j++) {
+				var addedDifference = Math.abs(newFriend.scores[j] - friendsArray[i].scores[j])
+
 				sumDifference = sumDifference + addedDifference;
 			}
+			console.log("This sumDifference is " + sumDifference)
 
 			if (lowestDifference === undefined) {
-				sumDifference = lowestDifference;
-				friendMatch = friends[i];
+				lowestDifference = sumDifference;
+				friendMatch = friendsArray[i];
+				console.log("The friendMatch so far is " + friendMatch.name);
 			} else if (sumDifference < lowestDifference) {
 				lowestDifference = sumDifference;
-				friendMatch = friends[i];
+				friendMatch = friendsArray[i];
+				console.log("The friendMatch so far is " + friendMatch.name);
+			} else {
+				console.log("The friendMatch so far is " + friendMatch.name);
 			}
 		}
 
-		console.log("Friend match:" + friendMatch);
+		console.log("The final friend match: " + friendMatch.name);
 
-		// var friendsArray = JSON.parse(friends);
+		friendsArray.push(newFriend);
 
-		// friendsArray.push(newfriend);
-
-		// console.log(friendsArray);
+		console.log(friendsArray);
 		
+		fs.writeFile(path.join(__dirname, '../data/friends.txt'), JSON.stringify(friendsArray), function (err) {
+			if (err) {
+		 		return console.error(err);
+			} 
+		 	console.log('friends.txt was updated');
+			});
+
+		})
 		res.status(200).send();
 	})
 }
