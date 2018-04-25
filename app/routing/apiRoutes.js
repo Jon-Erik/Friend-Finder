cfc7396da055d9377a399368c5298f8b4c312522
Jon-Erik <jdc151@case.edu>
2018-04-25 11:59:30 -0400

    added sample modal via bootstrap code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,52 +11,52 @@ module.exports = function(app) {
 
 		var friendsArray;
 
-		fs.readFile(path.join(__dirname,'../data/friends.txt'), 'utf8', function(err, data) {
-		if (err) {
-			return console.error(err);
-		}
-
-		friendsArray = JSON.parse(data);
-
 		var friendMatch;
-		var lowestDifference;
 
-		for (i = 0; i < friendsArray.length; i++) {
-			var sumDifference = 0;
-
-			for (j = 0; j < friendsArray[i].scores.length; j++) {
-				var addedDifference = Math.abs(newFriend.scores[j] - friendsArray[i].scores[j])
-
-				sumDifference = sumDifference + addedDifference;
-			}
-			console.log("This sumDifference is " + sumDifference)
-
-			if (lowestDifference === undefined) {
-				lowestDifference = sumDifference;
-				friendMatch = friendsArray[i];
-				console.log("The friendMatch so far is " + friendMatch.name);
-			} else if (sumDifference < lowestDifference) {
-				lowestDifference = sumDifference;
-				friendMatch = friendsArray[i];
-				console.log("The friendMatch so far is " + friendMatch.name);
-			} else {
-				console.log("The friendMatch so far is " + friendMatch.name);
-			}
-		}
-
-		console.log("The final friend match: " + friendMatch.name);
-
-		friendsArray.push(newFriend);
-
-		console.log(friendsArray);
-		
-		fs.writeFile(path.join(__dirname, '../data/friends.txt'), JSON.stringify(friendsArray), function (err) {
+		fs.readFile(path.join(__dirname,'../data/friends.txt'), 'utf8', function(err, data) {
 			if (err) {
-		 		return console.error(err);
-			} 
-		 	console.log('friends.txt was updated');
-			});
+				return console.error(err);
+			}
 
+			friendsArray = JSON.parse(data);
+
+			var lowestDifference;
+
+			for (i = 0; i < friendsArray.length; i++) {
+				var sumDifference = 0;
+
+				for (j = 0; j < friendsArray[i].scores.length; j++) {
+					var addedDifference = Math.abs(newFriend.scores[j] - friendsArray[i].scores[j])
+
+					sumDifference = sumDifference + addedDifference;
+				}
+				console.log("This sumDifference is " + sumDifference)
+
+				if (lowestDifference === undefined) {
+					lowestDifference = sumDifference;
+					friendMatch = friendsArray[i];
+					console.log("The friendMatch so far is " + friendMatch.name);
+				} else if (sumDifference < lowestDifference) {
+					lowestDifference = sumDifference;
+					friendMatch = friendsArray[i];
+					console.log("The friendMatch so far is " + friendMatch.name);
+				} else {
+					console.log("The friendMatch so far is " + friendMatch.name);
+				}
+			}
+
+			console.log("The final friend match: " + friendMatch.name);
+
+			friendsArray.push(newFriend);
+
+			console.log(friendsArray);
+			
+			fs.writeFile(path.join(__dirname, '../data/friends.txt'), JSON.stringify(friendsArray), function (err) {
+				if (err) {
+			 		return console.error(err);
+				} 
+			 	console.log('friends.txt was updated');
+				});
 		})
 		res.status(200).send();
 	})
